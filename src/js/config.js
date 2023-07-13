da2f2d97458b492fcf3a46c137602372e62f44dd
AbrorSoft <abroranvarov2@mail.ru>
2023-07-14 02:52:28 +0500

    final project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '6d4f0648-a50e-42f3-a9f8-9ab87aed4f57';
+export const MODAL_CLOSE_SEC = 2.5;
