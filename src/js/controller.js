b4ae3193a0a78accd543eecc0a546745046abf70
AbrorSoft <abroranvarov2@mail.ru>
2023-07-14 03:01:49 +0500

    removed unnecessary codes

--- src/js/controller.js
+++ src/js/controller.js
@@ -52,7 +52,7 @@ const controlSearchResults = async function () {
     // 4) Render initial pagination buttons
     paginationView.render(model.state.search);
   } catch (err) {
-    console.log(err);
+    alert(err);
   }
 };
 
@@ -95,7 +95,6 @@ const controlAddRecipe = async function (newRecipe) {
 
     // Upload the new recipe data
     await model.uploadRecipe(newRecipe);
-    console.log(model.state.recipe);
 
     // Render recipe
     recipeView.render(model.state.recipe);
